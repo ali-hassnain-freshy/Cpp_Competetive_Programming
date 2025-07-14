8d2f4eb1a73df669bb3038f1ac902ac9aa74036b
AliHassnainFutureLeaders <maherhassnain70@gmail.com>
2025-07-14 10:26:32 -0700

    as

---
+++ Day - 2/Control Flow/Practice Q&A/q1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    char ch; 
+    cout << endl << ">>";
+    cin  >> ch;
+
+    if (ch <= 'a' && ch >= 'z')
+    {
+        cout << "It's Lower";
+    }
+    else {
+        cout << "It's Upper";
+    }
+    
+    return 0;
+
+
+}
