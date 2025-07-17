9216090891c936173d258a454cdeb11c6dc32759
AliHassnainFutureLeaders <maherhassnain70@gmail.com>
2025-07-17 07:59:21 -0700

    gh

---
+++ Day - 3/Qs - Cf/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    for (int i = 2; i <= nan; i++)
+    {
+        if (n % 2 ==  0) 
+        {
+            cout << i << " is non prime" << endl;
+        } 
+        else 
+        {
+            cout << i << " is prime" << endl;
+        }
+        
+    }
+    
+
+    return 0;
+
+
+}
